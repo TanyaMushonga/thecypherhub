f357b7b3c140971e6e865a8295249fb23c5b4d0a
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-02-03 12:46:13 +0200

    Add authentication layout and pages for login and signup

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import LoginForm from "../../../components/othercomponents/LoginForm";
+import Link from "next/link";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Login",
+};
+
+function page() {
+  return (
+    <main className="flex justify-center items-center md:p-10 p-5 h-[90vh]">
+      <div className="h-fit max-h-[50rem] w-full max-w-[30rem] overflow-hidden bg-accent p-5">
+        <p className="text-center text-xl md:text-2xl font-bold">
+          Login in to your account
+        </p>
+        <div className="mt-5">
+          <LoginForm />
+          <p className="text-center my-3">Or</p>
+
+          <p className="text-center my-3">
+            Don&apos;t have an account?{" "}
+            <Link href="/signup" className="text-primary">
+              Signup
+            </Link>
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default page;
---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import SignupForm from "../../../components/othercomponents/Signup";
+
+function page() {
+  return (
+    <main className="flex justify-center items-center md:p-10 p-5">
+      <div className="h-fit max-h-[50rem] w-full max-w-[30rem] overflow-hidden bg-accent p-5">
+        <p className="text-center text-xl md:text-2xl font-bold">
+          Create an account
+        </p>
+        <div className="mt-5">
+          <SignupForm />
+          <p className="text-center my-3">Or</p>
+
+          <p className="text-center my-3">
+            Already have an account?{" "}
+            <a href="/login" className="text-primary">
+              Login
+            </a>
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default page;
