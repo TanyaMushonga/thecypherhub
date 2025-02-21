56dc240357bd726fd2db93e996ebc5f368dcd80e
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-02-21 19:46:30 +0200

    Remove logging configuration from PrismaClient initialization

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,9 +4,7 @@ const globalForPrisma = global as unknown as { prisma?: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["query", "info", "warn", "error"], 
-  });
+  new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
