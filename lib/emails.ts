79911c61d901340e3e13f808ebf7830be76d3e38
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-03-24 12:58:49 +0200

    Revamp subscription confirmation email template with enhanced styling and additional content

--- lib/emails.ts
+++ lib/emails.ts
@@ -9,18 +9,152 @@ export const sendConfirmationEmailAfterSubscribe = async (
   const confirmationLink = `${process.env.NEXT_PUBLIC_BASE_URL}/newsubscription?token=${token}`;
   try {
     const { data, error } = await resend.emails.send({
-      from: "newsletter@thecypherhub.tech", // Update with your registered domain
+      from: "TanyaMushonga@thecypherhub.tech",
       to: email,
       subject: "Thank you for subscribing to our newsletter!",
-      html: `
-        <div style="font-family: Arial, sans-serif; line-height: 1.6;">
-          <h2>Thank you for subscribing to our newsletter!</h2>
-          <p>We're excited to have you on board. Please confirm your subscription by clicking the link below:</p>
-          <a href="${confirmationLink}" style="display: inline-block; padding: 10px 20px; color: #fff; background-color: #007bff; text-decoration: none; border-radius: 5px;">Confirm Subscription</a>
-          <p>If you did not subscribe to our newsletter, please ignore this email.</p>
-          <p>Best regards,<br>The CypherHub Team</p>
-        </div>
-      `,
+      html: `<table
+      align="center"
+      width="600"
+      cellpadding="0"
+      cellspacing="0"
+      class="container"
+      style="
+        border: 1px solid #ddd;
+        border-radius: 10px;
+        background-color: black;
+        padding: 20px;
+      "
+    >
+      <tr>
+        <td align="center">
+          <img
+            src="https://www.thecypherhub.tech/fallback.webp"
+            alt="The CypherHub Logo"
+            style="max-width: 150px; margin-bottom: 20px"
+          />
+        </td>
+      </tr>
+      <tr>
+        <td align="center">
+          <h2 style="color: #fff">
+            Thank you for subscribing to our newsletter!
+          </h2>
+        </td>
+      </tr>
+      <tr>
+        <td style="font-style: italic; color: #555; text-align: center">
+          "The only way to do great work is to love what you do. Keep coding,
+          keep building!"
+        </td>
+      </tr>
+      <tr>
+        <td class="content">
+          <p style="color: #fff">
+            We're excited to have you on board. Please confirm your subscription
+            by clicking the link below:
+          </p>
+        </td>
+      </tr>
+      <tr>
+        <td align="center" style="margin: 20px 0">
+          <a
+            href="${confirmationLink}"
+            class="button"
+            style="
+              display: inline-block;
+              padding: 10px 20px;
+              color: #fff;
+              background-color: #274463;
+              text-decoration: none;
+              border-radius: 5px;
+            "
+            >Confirm Subscription</a
+          >
+        </td>
+      </tr>
+
+      <tr>
+        <td class="content">
+          <p style="color: #fff">
+            Welcome to The CypherHub Newsletter! You will be receiving weekly
+            emails about new articles, trending technology, coding tips, and
+            much more. Stay tuned for exciting updates and valuable content to
+            help you stay ahead in the tech world.
+          </p>
+        </td>
+      </tr>
+      <tr>
+        <td class="content">
+          <p style="color: #fff">
+            If you did not subscribe to our newsletter, please ignore this
+            email.
+          </p>
+        </td>
+      </tr>
+      <tr>
+        <td class="content">
+          <p style="color: #fff">Best regards,<br />Tanyaradzwa T Mushonga</p>
+        </td>
+      </tr>
+      <tr>
+        <td
+          align="center"
+          style="padding: 20px 0; margin-top: 20px; border-top: 1px solid #ddd; display: flex; justify-content: center"
+        >
+          <div>
+            <a
+              href="https://www.linkedin.com/in/tanyaradzwa-t-mushonga-b23745209/"
+              style="margin: 0 10px"
+            >
+              <img
+                src="https://www.thecypherhub.tech/linkedin.png"
+                alt=""
+                width="37"
+                height="37"
+                class="social-icons"
+              />
+            </a>
+          </div>
+          <div>
+            <a href="https://wa.me/+263712389290" style="margin: 0 10px">
+              <img
+                src="https://www.thecypherhub.tech/whatsapp.png"
+                alt=""
+                width="30"
+                height="30"
+                class="social-icons"
+              />
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://www.tanyaradzwatmushonga.me/"
+              style="margin: 0 10px"
+            >
+              <img
+                src="https://www.thecypherhub.tech/portifolio.png"
+                alt=""
+                width="33"
+                height="33"
+                class="social-icons"
+              />
+            </a>
+          </div>
+        </td>
+      </tr>
+      <tr>
+        <td align="center" style="margin-top: 20px; color: #fff">
+          <p style="color: #fff">Bulawayo, Zimbabwe</p>
+          <p>
+            <a
+              href="https://thecypherhub.tech"
+              style="color: #fff; text-decoration: underline"
+              >thecypherhub.tech</a
+            >
+          </p>
+        </td>
+      </tr>
+    </table>`,
     });
 
     if (error) {
