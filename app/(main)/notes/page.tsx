e8fd9688990fe0670c27bc7d12356c932cc827d5
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-03-25 15:55:07 +0200

    Add notes page component to display notes

---
+++ app/(main)/notes/page.tsx
@@ -0,0 +1,12 @@
+import Notes from "@/components/othercomponents/notes";
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      <Notes />
+    </div>
+  );
+}
+
+export default page;
