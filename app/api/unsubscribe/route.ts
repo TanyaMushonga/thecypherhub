ed9067a1cc3a44590ba41372626380e61f7553d9
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-03-28 21:42:16 +0200

    Add unsubscribe API route to handle subscriber unsubscription requests

---
+++ app/api/unsubscribe/route.ts
@@ -0,0 +1,38 @@
+import prisma from "@/lib/prisma";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const { email } = body;
+    if (!email) {
+      return new Response(JSON.stringify({ error: "The email is required" }), {
+        status: 400,
+      });
+    }
+    const subscriber = await prisma.subscribers.findUnique({
+      where: { email },
+    });
+    if (!subscriber) {
+      return new Response(JSON.stringify({ error: "Subscriber not found" }), {
+        status: 400,
+      });
+    }
+    const updatedSubscriber = await prisma.subscribers.update({
+      where: { email: subscriber.email },
+      data: { status: 0 },
+    });
+    if (updatedSubscriber) {
+      return new Response(
+        JSON.stringify({
+          message: "Unsubscribed successfully, We're sad to see you go.",
+        }),
+        { status: 200 }
+      );
+    }
+  } catch (error) {
+    console.error("Error unsubscribing:", error);
+    return new Response(JSON.stringify({ message: "Internal server error" }), {
+      status: 500,
+    });
+  }
+}
